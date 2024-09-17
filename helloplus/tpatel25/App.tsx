1912b147f7790fcbfff9d51401c69af8dcd91f95
Trishu Patel <pateltrishu28@gmail.com>
2024-09-17 09:44:22 -0400

    feat: Implement simple login in

--- helloplus/tpatel25/App.tsx
+++ helloplus/tpatel25/App.tsx
@@ -1,10 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { auth } from "./firebaseConfig";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { User } from "firebase/auth";
 
 export default function App() {
+  const [user, useUser] = useState<User>();
+  const [email, useEmail] = useState('');
+  const [password, usePassword] = useState('');
+
+  const signUp = () => {
+    if (email === undefined) {
+      console.log("Please type in a email");
+      return;
+    }
+
+    if (password === undefined) {
+      console.log("Please type in an password");
+      return;
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        useUser(userCredential.user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Email</Text>
+      <TextInput style={styles.input} onChangeText={useEmail} value={email} />
+      <Text>Password</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={usePassword}
+        value={password}
+      />
+      <Button title="Sign Up" onPress={signUp} />
+      {user && <Text>Hello, {user?.email}!</Text>}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +52,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: "50%",
+    marginHorizontal: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
