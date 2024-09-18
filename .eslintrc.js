b611f61a5abe31cb46ed68b7913355e2024589f1
Rudra Patel <patelrudra2003@gmail.com>
2024-09-18 15:34:35 -0400

    feat: Setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "prettier/prettier": "warn",
+  },
+};
