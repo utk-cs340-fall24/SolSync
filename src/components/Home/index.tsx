8d9bd6c1e32fbb7eb223de7fcb6457c82e906b90
Rudra Patel <patelrudra2003@gmail.com>
2024-09-18 16:50:04 -0400

    feat: Change formatting rules

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Home() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
+           <Text>Home!</Text>
     </View>
   );
 }
