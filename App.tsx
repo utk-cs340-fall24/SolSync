8305c752c2ad398a196aedbf533d1036bba807b5
Rudra Patel <patelrudra2003@gmail.com>
2024-09-23 19:28:23 -0400

    feat: Add icons for home and settings page

--- App.tsx
+++ App.tsx
@@ -4,13 +4,38 @@ import HomeScreen from "@/screens/HomeScreen";
 import SettingsScreen from "@/screens/SettingsScreen";
 import SignUpScreen from "@/screens/SignUpScreen";
 import LoginScreen from "@/screens/LoginScreen";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+function getIconName(
+  routeName: string,
+  focused: boolean
+): keyof typeof Ionicons.glyphMap {
+  switch (routeName) {
+    case "Home":
+      return focused ? "home" : "home-outline";
+    case "Settings":
+      return focused ? "settings" : "settings-outline";
+    default:
+      return "caret-up";
+  }
+}
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            const iconName = getIconName(route.name, focused);
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "purple",
+          tabBarInactiveTintColor: "gray",
+          headerShown: false,
+        })}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
         <Tab.Screen name="Sign Up" component={SignUpScreen} />
