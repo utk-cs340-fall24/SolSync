97fdc5f515f5a17178d86bebfb44562993909fa4
Rudra Patel <patelrudra2003@gmail.com>
2024-10-01 19:17:55 -0400

    feat: Fix PR issues

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,6 +1,4 @@
 import Profile from "@/components/Profile";
-import AuthorizedProfile from "@/components/Profile/AuthorizedProfile";
-import useUser from "@/hooks/useUser";
 
 export default function ProfileScreen() {
   // const user = useUser();
