c3bbd8df08634ca27f493ff2ae0239b9ed11305a
amy-huang16 <ahuang16@vols.utk.edu>
2024-10-13 21:30:31 -0400

    merge log in page with create an account button, styled log in

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -10,7 +10,6 @@ export type ProfileStackParamList = {
   LogIn: undefined;
   SignUp: undefined;
   AuthorizedProfile: undefined;
-  UnauthorizedProfile: undefined;
 };
 
 const Stack = createNativeStackNavigator<ProfileStackParamList>();
@@ -29,12 +28,11 @@ export default function Profile() {
       ) : (
         <>
           <Stack.Screen
-            name="UnauthorizedProfile"
-            component={UnauthorizedProfile}
+            name="LogIn"
+            component={LogIn}
             options={{ headerShown: false }}
           />
-          <Stack.Screen name="LogIn" component={LogIn} />
-          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="SignUp" component={SignUp}  />
         </>
       )}
     </Stack.Navigator>
