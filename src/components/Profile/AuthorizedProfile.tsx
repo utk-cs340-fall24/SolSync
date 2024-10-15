b35cacae480ff0f0cfaefbad15c56af0b11eeb60
amy-huang16 <141398928+amy-huang16@users.noreply.github.com>
2024-10-14 22:07:53 -0400

    feat: style profile page and add functionality to buttons  (#61)

--- src/components/Profile/AuthorizedProfile.tsx
+++ src/components/Profile/AuthorizedProfile.tsx
@@ -1,112 +1,209 @@
 import { signOut } from "firebase/auth";
 import { useState } from "react";
-import { StyleSheet, View, Text, Button, Switch } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Modal,
+  Alert,
+  Pressable,
+} from "react-native";
+import { default as FAIcon } from "react-native-vector-icons/FontAwesome";
 import { firebaseAuth } from "../../../firebaseConfig";
+import getLocationFromDevice from "@/utils/getLocationFromDevice";
+import { LinearGradient } from "expo-linear-gradient";
+import { default as FeatherIcon } from "react-native-vector-icons/Feather";
+import useUser from "@/hooks/useUser";
+
+const gradientColors = ["#FFD18A", "#C6B9E4", "#81A8F4"];
+const colorsLocations = [0.15, 0.65, 1];
 
 export default function AuthorizedProfile() {
-  const [pushIsEnabled, setPushIsEnabled] = useState(false);
-  const pushNotificationsSwitch = () => {
-    setPushIsEnabled((previousState) => !previousState);
-    if (!pushIsEnabled) {
-      console.log("Push notifications are ON");
-    } else {
-      console.log("Push notifications are OFF");
-    }
-  };
-
-  const [emailIsEnabled, setEmailIsEnabled] = useState(false);
-  const emailNotificationsSwitch = () => {
-    setEmailIsEnabled((previousState) => !previousState);
-    if (!emailIsEnabled) {
-      console.log("Email notifications are ON");
-    } else {
-      console.log("Email notifications are OFF");
-    }
-  };
-
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
-    <View style={styles.page}>
-      <Text style={styles.header}>Hello, Amy!</Text>
-      <Icon
-        name="user-circle"
-        size={100}
-        color="gray"
-        style={{ marginBottom: 20 }}
-      ></Icon>
-      <View style={styles.option}>
-        <Text>Name: </Text>
-        <Text>Amy Huang</Text>
-      </View>
-      <View style={styles.option}>
-        <Text>Email: </Text>
-        <Text>filleremail@gmail.com</Text>
-      </View>
-      <View style={styles.option}>
-        <Text>Push Notifications</Text>
-        <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={pushIsEnabled ? "#f5dd4b" : "#f4f3f4"}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={pushNotificationsSwitch}
-          value={pushIsEnabled}
-        />
-      </View>
-      <View style={[styles.option, { marginBottom: 20 }]}>
-        <Text>Email Notifications</Text>
-        <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={emailIsEnabled ? "#f5dd4b" : "#f4f3f4"}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={emailNotificationsSwitch}
-          value={emailIsEnabled}
-        />
-      </View>
-      <View style={styles.button}>
-        <Button
-          title="Reset Location"
-          color="white"
-          onPress={() => {
-            console.log("Reset Location");
+    <LinearGradient
+      colors={gradientColors}
+      locations={colorsLocations}
+      style={styles.container}
+    >
+      <View style={styles.content}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setIsModalVisible(!isModalVisible);
           }}
-        ></Button>
+        >
+          <View style={styles.content}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Edit Name and Email here</Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setIsModalVisible(!isModalVisible)}
+              >
+                <Text style={styles.textStyle}>Save</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableOpacity style={styles.editButton}>
+          <FeatherIcon
+            name="edit"
+            size={38}
+            color="white"
+            onPress={() => setIsModalVisible(true)}
+          ></FeatherIcon>
+        </TouchableOpacity>
+
+        <Text style={styles.header}>Hello, Amy!</Text>
+
+        <FAIcon
+          name="user-circle"
+          size={100}
+          color="black"
+          style={{ marginBottom: 60 }}
+        ></FAIcon>
+
+        <View style={styles.option}>
+          <Text style={{ width: 70 }}>Name: </Text>
+          <Text>Amy</Text>
+        </View>
+        <View style={styles.line}></View>
+        <View style={styles.option}>
+          <Text style={{ width: 70 }}>Email: </Text>
+          <Text>{useUser()?.email}</Text>
+        </View>
+
+        <TouchableOpacity
+          style={styles.locationButton}
+          onPress={() => getLocationFromDevice()}
+        >
+          <FAIcon
+            name="location-arrow"
+            size={25}
+            color="white"
+            style={{ marginHorizontal: 6 }}
+          ></FAIcon>
+          <Text style={styles.buttonText}>Reset Your Location</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.logOutButton}
+          onPress={() => signOut(firebaseAuth)}
+        >
+          <FAIcon
+            name="sign-out"
+            size={25}
+            color="white"
+            style={{ marginHorizontal: 6 }}
+          ></FAIcon>
+          <Text style={styles.buttonText}>Log Out</Text>
+        </TouchableOpacity>
       </View>
-      <View style={styles.button}>
-        <Button
-          title="Log Out"
-          color="white"
-          onPress={() => {
-            signOut(firebaseAuth);
-          }}
-        ></Button>
-      </View>
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  page: {
+  container: {
     flex: 1,
-    backgroundColor: "white",
+  },
+  content: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    marginTop: 20,
+  },
+  editButton: {
+    position: "absolute",
+    top: "5%",
+    right: "8%",
   },
   header: {
     fontSize: 30,
-    paddingBottom: 20,
+    paddingBottom: 60,
   },
   option: {
     margin: 10,
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "flex-start",
     alignItems: "center",
     gap: 10,
     width: "80%",
   },
+  line: {
+    height: 1,
+    width: "80%",
+    backgroundColor: "#918E8E",
+    marginVertical: 14,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
+    marginHorizontal: 12,
+  },
+  locationButton: {
+    backgroundColor: "#908BE8",
+    width: "64%",
+    paddingVertical: 10,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 60,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  logOutButton: {
+    backgroundColor: "#908BE8",
+    width: "64%",
+    paddingVertical: 10,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 24,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  // modal stuff starting here
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    // padding: 100,
+    width: "92%",
+    height: "92%",
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 4,
+  },
   button: {
-    borderRadius: 10,
-    backgroundColor: "purple",
-    margin: 10,
-    width: "40%",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
