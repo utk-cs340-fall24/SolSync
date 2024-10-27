4f2eb515abecf2df947014edc43c64f95c96ccc8
Trishu Patel <pateltrishu28@gmail.com>
2024-10-27 11:24:07 -0400

    refactor: Use env file for firebase admin credentials

--- lambdas/firebase-demo/index.mjs
+++ lambdas/firebase-demo/index.mjs
@@ -1,15 +1,23 @@
+import dayjs from "dayjs";
 import { cert, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import { promises as fs } from "fs";
-import dayjs from "dayjs";
-
-const serviceAccountPath = "./lambdas/firebase-demo/serviceAccount.json";
-const serviceAccount = JSON.parse(
-  await fs.readFile(serviceAccountPath, "utf-8"),
-);
+import { URL } from "url";
 
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    type: process.env.FIREBASE_ADMIN_TYPE,
+    project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_id,
+    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+    token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_ADMIN_UNIVERSE_DOMAIN,
+  }),
 });
 
 const db = getFirestore();
@@ -211,8 +219,8 @@ export const handler = async () => {
 
     await sendDailyEmail(user, emailBody);
 
-    console.log(`Email sent to ${user.displayName} at ${user.email}`)
+    console.log(`Email sent to ${user.displayName} at ${user.email}`);
   });
 };
 
-handler()
+handler();
